e26a4e42d5befd8b86df42a97d63fe5c58ef588c
NS-Sheam <123sheamfeni@gmail.com>
2024-01-25 13:41:41 +0600

    Add product routes

---
+++ src/app/modules/product/product.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { ProductControllers } from "./product.controller";
+
+const router = Router();
+router.post("/", ProductControllers.createProduct);
+router.get("/", ProductControllers.getAllProducts);
+router.get("/:id", ProductControllers.getSingleProduct);
+router.patch("/:id", ProductControllers.updateProduct);
+router.delete("/:id", ProductControllers.deleteProduct);
+
+export const productRoutes = router;
