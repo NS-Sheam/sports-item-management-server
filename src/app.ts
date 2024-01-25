0f91f6a2116c982de491a79b6f06e2b9c1344a43
NS-Sheam <123sheamfeni@gmail.com>
2024-01-25 12:07:12 +0600

    Add api routes to app.js

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import router from "./app/routes";
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/api", router);
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
---
+++ src/app/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+const moduleRoutes = [
+  {
+    path: "/",
+    route: "",
+  },
+];
+
+moduleRoutes.forEach(({ path, route }) => router.use(path, route));
+
+export default router;
