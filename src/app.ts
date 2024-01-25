c072485144f042d504a3fe67b31a18ec4a10725a
NS-Sheam <123sheamfeni@gmail.com>
2024-01-25 11:15:36 +0600

    Update Prettier configuration and add cors type definitions

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
-import express from "express"
-const app = express()
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+  res.send("Hello World!");
+});
 
-export default app
+export default app;
