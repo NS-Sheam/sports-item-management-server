9d8fd35d87fdaa0007a8ff885bdfa804224b7fd7
NS-Sheam <123sheamfeni@gmail.com>
2024-01-25 13:39:56 +0600

    Add product controller

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,11 +1,65 @@
 import { Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
 import { ProductServices } from "./product.service";
+import sendResponse from "../../utils/sendResponse";
 
 const createProduct = catchAsync(async (req: Request, res: Response) => {
   const result = await ProductServices.createProduct(req.body);
-  res.status(201).json({
+  sendResponse(res, {
     success: true,
+    statusCode: 200,
+    message: "Product created successfully",
     data: result,
   });
 });
+
+const getAllProducts = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProductServices.getAllProducts();
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "All products fetched successfully",
+    data: result,
+  });
+});
+
+const getSingleProduct = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ProductServices.getSingleProduct(id);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Product fetched successfully",
+    data: result,
+  });
+});
+
+const updateProduct = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ProductServices.updateProduct(id, req.body);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Product updated successfully",
+    data: result,
+  });
+});
+
+const deleteProduct = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ProductServices.deleteProduct(id);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Product deleted successfully",
+    data: result,
+  });
+});
+
+export const ProductControllers = {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+};
